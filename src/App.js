f41ba716f107a7a05c4cd86b337a8ff242b7d6a3
zarriy <zarriy@outlook.com>
2022-11-18 20:38:58 +0500

    commiting the code to the 2nd branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Header from "./header";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/header.js
@@ -0,0 +1,23 @@
+import logo from "./media/logo.png";
+function Header() {
+  return (
+    <>
+      <header className="flex flex-row justify-between items-center mx-12 my-2 h-20 text-white">
+        <div>
+          <img src={logo} width="80px" height="80px" alt="Website Logo" />
+        </div>
+        <div>
+          <ul className="nav-elements flex flex-row gap-20 font-medium text-lg">
+            <li>About</li>
+            <li>Shop</li>
+            <li className="text-2xl">
+              <ion-icon name="cart-outline"></ion-icon>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
